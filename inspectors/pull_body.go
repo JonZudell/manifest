045fdf042db2dd48f3d6432b594148b83cb5e627
Blake Williams <blake@blakewilliams.me>
2024-12-18 12:58:07 -0500

    Remove testing parameter from pull body

--- inspectors/pull_body.go
+++ inspectors/pull_body.go
@@ -15,8 +15,5 @@ func PullBody(entry *manifest.Import, r *manifest.Result) error {
 		r.Error("It looks like your pull request description is empty! Please provide a description of your changes.")
 	}
 
-	// for testing purposes
-	r.WarnLine("inspectors/pull_body.go", "RIGHT", 20, "This is a warning")
-
 	return nil
 }
